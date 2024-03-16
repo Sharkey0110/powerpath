7e8918e51e49783e02da6de20c962f1f78ecf635
Jack Sharkey <113288400+Sharkey0110@users.noreply.github.com>
2024-03-16 18:41:40 +0000

    fix part 2

--- lib/database/models/user.model.ts
+++ lib/database/models/user.model.ts
@@ -2,6 +2,7 @@ import { Schema, models, model, Document } from "mongoose";
 
 export interface IUser extends Document{
   _id: string;
+  clerkId: string;
   email: string;
   username: string;
   firstName: string;
